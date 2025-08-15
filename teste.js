ad97a2eb8c7ff506b8e1900da972d1f2e6585ee1
Pedro <pedrogabriel18082003@gmail.com>
2025-08-15 15:12:27 -0300

    Criação de arquivo pra listar tarefas

--- teste.js
+++ teste.js
@@ -1,8 +1,33 @@
-let nome; //name
-let saudaçao; //message to user
+let tarefas = [];
 
-nome = "pedro";
-sauadacao = "eaee";
+function adicionarTarefa(tarefa) {
+  tarefas.push(tarefa);
+  console.log(`Tarefa adicionada: "${tarefa}"`);
+}
 
-alert(saudacao + nome);
+function removerTarefa(indice) {
+  if (indice >= 0 && indice < tarefas.length) {
+    const removida = tarefas.splice(indice, 1);
+    console.log(`Tarefa removida: "${removida[0]}"`);
+  } else {
+    console.log("Índice inválido. Nenhuma tarefa removida.");
+  }
+}
 
+function listarTarefas() {
+  console.log("Lista de Tarefas:");
+  if (tarefas.length === 0) {
+    console.log("Nenhuma tarefa adicionada.");
+  } else {
+    tarefas.forEach((tarefa, index) => {
+      console.log(`${index}: ${tarefa}`);
+    });
+  }
+}
+
+
+adicionarTarefa("Estudar JavaScript");
+adicionarTarefa("Fazer exercícios");
+listarTarefas();
+removerTarefa(0);
+listarTarefas();
